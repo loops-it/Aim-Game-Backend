0e689e91fe64280d855aac57e6b65366d4186230
madushan10 <mnmnjith@gmail.com>
2025-01-22 16:44:16 +0530

    new db update 3

--- databases/index.js
+++ databases/index.js
@@ -5,7 +5,7 @@ module.exports.connect = async () => {
   try {
     const data = await mongoose.connect(
       // `mongodb+srv://${database.user}:${database.password}@${database.host}/${database.database}?retryWrites=true&w=majority`
-      `mongodb+srv://dgnayanajith:CdOqhYZJEzZJlfZO@medcube.qvekw.mongodb.net/?retryWrites=true&w=majority&appName=MedCube&`
+      `mongodb+srv://dgnayanajith:CdOqhYZJEzZJlfZO@medcube.qvekw.mongodb.net/?retryWrites=true&w=majority&appName=AimGame&`
     );
     console.log(`-----Connected to ${database.database} successfully-----`);
   } catch (error) {
