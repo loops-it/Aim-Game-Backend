3c6183cdd6431a2a59b0b44e7e0fc92a5d88c3c3
madushan10 <mnmnjith@gmail.com>
2025-01-22 16:41:53 +0530

    new db update 2

--- databases/index.js
+++ databases/index.js
@@ -5,7 +5,7 @@ module.exports.connect = async () => {
   try {
     const data = await mongoose.connect(
       // `mongodb+srv://${database.user}:${database.password}@${database.host}/${database.database}?retryWrites=true&w=majority`
-      `mongodb+srv://dgnayanajith:CdOqhYZJEzZJlfZO@medcube.qvekw.mongodb.net/MedCube?retryWrites=true&w=majority`
+      `mongodb+srv://dgnayanajith:CdOqhYZJEzZJlfZO@medcube.qvekw.mongodb.net/?retryWrites=true&w=majority&appName=MedCube&`
     );
     console.log(`-----Connected to ${database.database} successfully-----`);
   } catch (error) {
