533f064e492ea98566ba2fa5379b3e3e875b99a9
madushan10 <mnmnjith@gmail.com>
2025-02-03 14:11:15 +0530

    db timeout issue

--- databases/index.js
+++ databases/index.js
@@ -3,10 +3,16 @@ const { database } = require("../config");
 
 module.exports.connect = async () => {
   try {
-    const data = await mongoose.connect(
-      // `mongodb+srv://${database.user}:${database.password}@${database.host}/${database.database}?retryWrites=true&w=majority`
-      `mongodb+srv://dgnayanajith:CdOqhYZJEzZJlfZO@medcube.qvekw.mongodb.net/AimGame?retryWrites=true&w=majority`
-    );
+    // const data = await mongoose.connect(
+    //   // `mongodb+srv://${database.user}:${database.password}@${database.host}/${database.database}?retryWrites=true&w=majority`
+    //   `mongodb+srv://dgnayanajith:CdOqhYZJEzZJlfZO@medcube.qvekw.mongodb.net/AimGame?retryWrites=true&w=majority`
+    // );
+    mongoose.connect('mongodb://username:password@host:port/database', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 10000,
+  });
+  
     console.log(`-----Connected to ${database.database} successfully-----`);
   } catch (error) {
     console.log("db connection error ", { error });
